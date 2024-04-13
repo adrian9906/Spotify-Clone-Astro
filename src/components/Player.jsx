ea35716d5eae862b316c5456d1b032188c5d73ed
Adrian Fernandez <adriandfl99@gmail.com>
2024-04-13 01:50:45 -0400

    Updated svgs.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -18,7 +18,49 @@ export const Lyrics = ({ classname }) => (
   </svg>
 )
 
+export const Random = ({ classname }) => (
 
+  <svg role="img"
+    height="16"
+    width="16"
+    fill="currentColor"
+    aria-hidden="true"
+    aria-label="Aleatorio"
+    viewBox="0 0 16 16">
+    <path d="M13.151.922a.75.75 0 1 0-1.06 1.06L13.109 3H11.16a3.75 3.75 0 0 0-2.873 1.34l-6.173 7.356A2.25 2.25 0 0 1 .39 12.5H0V14h.391a3.75 3.75 0 0 0 2.873-1.34l6.173-7.356a2.25 2.25 0 0 1 1.724-.804h1.947l-1.017 1.018a.75.75 0 0 0 1.06 1.06L15.98 3.75 13.15.922zM.391 3.5H0V2h.391c1.109 0 2.16.49 2.873 1.34L4.89 5.277l-.979 1.167-1.796-2.14A2.25 2.25 0 0 0 .39 3.5z">
+    </path>
+    <path d="m7.5 10.723.98-1.167.957 1.14a2.25 2.25 0 0 0 1.724.804h1.947l-1.017-1.018a.75.75 0 1 1 1.06-1.06l2.829 2.828-2.829 2.828a.75.75 0 1 1-1.06-1.06L13.109 13H11.16a3.75 3.75 0 0 1-2.873-1.34l-.787-.938z">
+
+    </path>
+  </svg>
+)
+
+
+export const Repeat = ({ classname }) => (
+
+  <svg role="img"
+    height="16"
+    width="16"
+    fill="currentColor"
+    aria-hidden="true"
+    aria-label="Repetir la cancion"
+    viewBox="0 0 16 16">
+    <path d="M0 4.75A3.75 3.75 0 0 1 3.75 1h8.5A3.75 3.75 0 0 1 16 4.75v5a3.75 3.75 0 0 1-3.75 3.75H9.81l1.018 1.018a.75.75 0 1 1-1.06 1.06L6.939 12.75l2.829-2.828a.75.75 0 1 1 1.06 1.06L9.811 12h2.439a2.25 2.25 0 0 0 2.25-2.25v-5a2.25 2.25 0 0 0-2.25-2.25h-8.5A2.25 2.25 0 0 0 1.5 4.75v5A2.25 2.25 0 0 0 3.75 12H5v1.5H3.75A3.75 3.75 0 0 1 0 9.75v-5z">
+    </path>
+  </svg>
+)
+
+export const Queue = ({className}) =>(
+  <svg role="img"
+  height="16"
+  width="16"
+  fill="currentColor"
+  aria-hidden="true"
+  aria-label="Cola"
+  viewBox="0 0 16 16">
+    <path d="M15 15H1v-1.5h14V15zm0-4.5H1V9h14v1.5zm-14-7A2.5 2.5 0 0 1 3.5 1h9a2.5 2.5 0 0 1 0 5h-9A2.5 2.5 0 0 1 1 3.5zm2.5-1a1 1 0 0 0 0 2h9a1 1 0 1 0 0-2h-9z"></path>
+  </svg>
+)
 export const Pause = ({ className }) => (
   <svg
     className={className}
@@ -311,6 +353,23 @@ export function Player() {
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center flex-col items-center">
           <div className="flex items-center gap-8">
+          <TooltipProvider delayDuration={300}>
+              <Tooltip>
+                <TooltipTrigger>
+                  <button
+                    className="text-[#b3b3b3] hover:text-white"
+                  >
+                    <Random />
+                  </button>
+
+                </TooltipTrigger>
+                <TooltipContent className="bg-gray-900">
+                  <p>Random music</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+
+            
             <TooltipProvider delayDuration={300}>
               <Tooltip>
                 <TooltipTrigger>
@@ -358,6 +417,22 @@ export function Player() {
                 </TooltipContent>
               </Tooltip>
             </TooltipProvider>
+
+
+            <TooltipProvider delayDuration={300}>
+              <Tooltip>
+                <TooltipTrigger>
+                <button
+              className="text-[#b3b3b3] hover:text-white"
+            >
+              <Repeat />
+            </button>
+                </TooltipTrigger>
+                <TooltipContent className="bg-gray-900">
+                  Repeat
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
           <SongControl audio={audioRef} />
         </div>
@@ -378,6 +453,13 @@ export function Player() {
         >
           <span>
             <Lyrics />
+          </span>
+        </button>
+
+        <button
+        >
+          <span>
+            <Queue />
           </span>
         </button>
         <VolumeControl />
