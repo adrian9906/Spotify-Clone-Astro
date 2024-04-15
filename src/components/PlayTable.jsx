da7c98639f7e891c2a8b513373eb3166c272246e
Adrian Fernandez <adriandfl99@gmail.com>
2024-04-15 09:28:24 -0400

    PlaySongTable and PauseSongTable svg added in PlayTable component.

--- src/components/PlayTable.jsx
+++ src/components/PlayTable.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 import { Pause, Play } from "./Player";
 import { usePlayerStore } from "@/store/playerStore";
 
+export const PlaySongTable = ()=>(
+    <svg 
+    role="img"
+    height="24"
+    width="24"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    fill="currentColor">
+      <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
+    </svg>
+  )
+export const PauseSongTable = ()=>(
+    <svg
+    role="img"
+    height="24"
+    width="24"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    fill="currentColor">
+        <path d='M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7H5.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7h-2.6z'>
+
+        </path>
+    </svg>
+)
+
+
 const PlayTable = ({id, idSong}) => {
     const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
@@ -25,9 +51,9 @@ const PlayTable = ({id, idSong}) => {
     return (
         <button onClick={handleClick} className="play-button">
             {isPlayingPlaylist ? (
-        <Pause />
+        <PauseSongTable />
       ) : (
-        <Play />
+        <PlaySongTable />
       )}
         </button>
     )
